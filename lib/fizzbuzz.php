11b09d4b0f9998f98c8f58cc8836525a6df64eb0
Sophie Gill <sgill@andigital.com>
2017-03-16 11:01:57 +0000

    refactors code to improve readability

--- lib/fizzbuzz.php
+++ lib/fizzbuzz.php
@@ -3,17 +3,22 @@
 class FizzBuzz {
 
   public function play($number) {
-    $result = "";
-    if ($number % 3 == 0) {
-      $result = $result . "Fizz";
+    $string = "";
+    if ($this->isDivisibleByThree($number)) {
+      $string .= "Fizz";
     }
-    if ($number % 5 == 0) {
-      $result = $result . "Buzz";
+    if ($this->isDivisibleByFive($number)) {
+      $string .= "Buzz";
     }
-    if ($result == "") {
-      return $number;
-    }
-    return $result;
+    return $string ? $string : $number;
+  }
+
+  private function isDivisibleByThree($number) {
+    return $number % 3 === 0;
+  }
+
+  private function isDivisibleByFive($number) {
+    return $number % 5 === 0;
   }
 
 }
